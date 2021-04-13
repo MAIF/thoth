175e7f63eebe988d80d7bb34ff5961fd0e30bc55
Alexandre Delègue <alexandre.delegue@maif.fr>
2021-04-13 09:18:25 +0200

    Update deps :  * upgrade akka  * upgrade vavr

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,9 +1,9 @@
 object Dependencies {
   val alpakkaKafkaVersion   = "2.0.7"
   val jacksonVersion        = "2.10.5"
-  val akkaVersion           = "2.6.6"
-  val vavrVersion           = "0.10.0"
+  val akkaVersion           = "2.6.14"
+  val vavrVersion           = "0.10.3"
   val jooqVersion           = "3.14.3"
   val jooqAsyncVersion      = "1.1.0"
-  val functionalJsonVersion = "1.0.1"
+  val functionalJsonVersion = "1.0.2"
 }
