459d58ef1bd6d979546278132e2b41e24ad7cd5b
Alexandre Delègue <adelegue@hotmail.com>
2026-04-01 20:35:49 +0200

    Migration to : * jackson 3 * jooq 3.21 Remove akka for pekko

--- thoth-jooq-reactor/src/test/java/fr/maif/eventsourcing/DefaultReactorAggregateStoreTest.java
+++ thoth-jooq-reactor/src/test/java/fr/maif/eventsourcing/DefaultReactorAggregateStoreTest.java
@@ -16,7 +16,7 @@ import java.util.function.Function;
 
 import static fr.maif.eventsourcing.AbstractPostgresEventStoreTest.eventEnvelope;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
